a0c1db508cd33cb3fc3926a8d86e2587af1728de
Tyler Romeo <tyler.romeo@willowtreeapps.com>
2015-05-08 12:47:26 -0400

    formatting fixes

--- src/main/scala/com/example/MyService.scala
+++ src/main/scala/com/example/MyService.scala
@@ -44,11 +44,15 @@ trait MyService extends HttpService {
           complete {
             <html>
               <body>
-                <h1>Say hello to <i>spray-routing</i> on <i>spray-can</i>!</h1>
+                <h1>Say hello to
+                  <i>spray-routing</i>
+                  on
+                  <i>spray-can</i>
+                  !</h1>
               </body>
             </html>
           }
+        }
       }
-    }
     }
 }
