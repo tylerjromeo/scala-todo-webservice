24f6720fb6fc656f0747c3811b6df4acee40f8c8
Tyler Romeo <tyler.romeo@willowtreeapps.com>
2015-05-13 18:37:17 -0700

    adding schema creation to boot

--- src/main/scala/com/example/Boot.scala
+++ src/main/scala/com/example/Boot.scala
@@ -2,6 +2,8 @@ package com.example
 
 import akka.actor.{ActorSystem, Props}
 import akka.io.IO
+import com.example.jdbc.TaskDAO
+import com.example.jdbc.TaskDAO.Task
 import spray.can.Http
 import akka.pattern.ask
 import akka.util.Timeout
@@ -17,5 +19,7 @@ object Boot extends App {
 
   implicit val timeout = Timeout(5.seconds)
   // start a new HTTP server on port 8080 with our service actor as the handler
+  val testData = Seq(Task(Some(1), "Do the first thing", complete = true), Task(Some(2), "Do the second thing", complete = false), Task(Some(3), "Do the third thing", complete = false))
+  TaskDAO.setup(testData)
   IO(Http) ? Http.Bind(service, interface = "localhost", port = 8080)
 }
